4c465ecab2aadff879f5d74d11d2c61102859ad9
Ben Ali Faker <fba@xanweb.com>
2024-07-04 09:28:28 +0100

    initial version

---
+++ src/main/java/ben_ali/faker/carrefour_delivery/carrefourdelivery/CarrefourDeliveryApplication.java
@@ -0,0 +1,13 @@
+package ben_ali.faker.carrefour_delivery.carrefourdelivery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarrefourDeliveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CarrefourDeliveryApplication.class, args);
+    }
+
+}
